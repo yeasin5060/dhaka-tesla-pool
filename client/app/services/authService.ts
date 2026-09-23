23f547ed71904c2f019fcec027dce8b2ab108ae9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-23 08:12:27 +0600

    services authservice.ts file complete

---
+++ client/app/services/authService.ts
@@ -0,0 +1,42 @@
+
+import type {
+  AuthResponse,
+  LoginData,
+  RegisterData,
+  User,
+} from "../types/auth";
+import axiosInstance from "../lib/axios";
+
+export const registerUser = async (
+  data: RegisterData
+): Promise<AuthResponse> => {
+  const response =
+    await axiosInstance.post<AuthResponse>(
+      "/auth/register",
+      data
+    );
+
+  return response.data;
+};
+
+export const loginUser = async (
+  data: LoginData
+): Promise<AuthResponse> => {
+  const response =
+    await axiosInstance.post<AuthResponse>(
+      "/auth/login",
+      data
+    );
+
+  return response.data;
+};
+
+export const getCurrentUser =
+  async (): Promise<User> => {
+    const response =
+      await axiosInstance.get<{
+        user: User;
+      }>("/auth/me");
+
+    return response.data.user;
+  };
