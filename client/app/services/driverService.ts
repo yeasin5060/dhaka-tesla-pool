31562e1198ca0ae5e758142cb7e5b09fb7a3a85c
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-23 08:14:54 +0600

    services driverservice.ts file complete

---
+++ client/app/services/driverService.ts
@@ -0,0 +1,94 @@
+import axiosInstance from "../lib/axios";
+
+import type {
+  DriverRequest,
+} from "../types/driver";
+
+import type { Pool, Vehicle } from "../types/pool";
+import type { RideRequest } from "../types/ride";
+
+export const toggleDriverOnline =
+  async (
+    isOnline: boolean
+  ): Promise<Vehicle> => {
+    const response =
+      await axiosInstance.patch<{
+        vehicle: Vehicle;
+      }>("/driver/online", {
+        isOnline,
+      });
+
+    return response.data.vehicle;
+  };
+
+export const getDriverRequests =
+  async (): Promise<DriverRequest[]> => {
+    const response =
+      await axiosInstance.get<{
+        rides: DriverRequest[];
+      }>("/driver/requests");
+
+    return response.data.rides;
+  };
+
+export const acceptRide = async (
+  rideId: string
+): Promise<RideRequest> => {
+  const response =
+    await axiosInstance.post<{
+      ride: RideRequest;
+    }>(
+      `/driver/rides/${rideId}/accept`
+    );
+
+  return response.data.ride;
+};
+
+export const arriveAtRide = async (
+  rideId: string
+) => {
+  const response =
+    await axiosInstance.post<{
+      ride: RideRequest;
+    }>(
+      `/driver/rides/${rideId}/arrive`
+    );
+
+  return response.data.ride;
+};
+
+export const startRide = async (
+  rideId: string
+) => {
+  const response =
+    await axiosInstance.post<{
+      ride: RideRequest;
+    }>(
+      `/driver/rides/${rideId}/start`
+    );
+
+  return response.data.ride;
+};
+
+export const completeRide = async (
+  rideId: string
+) => {
+  const response =
+    await axiosInstance.post<{
+      ride: RideRequest;
+    }>(
+      `/driver/rides/${rideId}/complete`
+    );
+
+  return response.data.ride;
+};
+
+export const getCurrentPool =
+  async (): Promise<Pool | null> => {
+    const response =
+      await axiosInstance.get<{
+        pool: Pool | null;
+      }>("/driver/current-pool");
+
+    return response.data.pool;
+  };
