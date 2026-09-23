789008ff64a0801c865a5a266ef5344df2ebf5c5
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-23 08:13:35 +0600

    services rideservice.ts file complete

---
+++ client/app/services/rideService.ts
@@ -0,0 +1,50 @@
+import axiosInstance from "../lib/axios";
+
+import type {
+  CreateRideData,
+  RideRequest,
+} from "../types/ride";
+
+export const createRide = async (
+  data: CreateRideData
+) => {
+  const response =
+    await axiosInstance.post<{
+      message: string;
+      ride: RideRequest;
+    }>("/rides", data);
+
+  return response.data;
+};
+
+export const getMyRides = async () => {
+  const response =
+    await axiosInstance.get<{
+      rides: RideRequest[];
+    }>("/rides");
+
+  return response.data.rides;
+};
+
+export const getRideById = async (
+  id: string
+) => {
+  const response =
+    await axiosInstance.get<{
+      ride: RideRequest;
+    }>(`/rides/${id}`);
+
+  return response.data.ride;
+};
+
+export const cancelRide = async (
+  id: string
+) => {
+  const response =
+    await axiosInstance.patch<{
+      message: string;
+      ride: RideRequest;
+    }>(`/rides/${id}/cancel`);
+
+  return response.data;
+};
